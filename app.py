30c1da9ece3af62a913402bb0793cf4777054b38
cheguevara619 <luisqf@protonmail.com>
2026-03-30 18:56:21 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Teste")
+
+def fetch_leads():
+leads = []
+leads.append("Funciona!")
+return leads
+
+if st.button("Testar"):
+results = fetch_leads()
+for r in results:
+st.write(r)
